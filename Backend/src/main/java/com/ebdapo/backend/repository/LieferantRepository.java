af7279ce4a7d4c0ec8e3b96541e03aaa747013cd
Miriam Lang <miriam.lang@student.reutlingen-university.de>
2020-11-19 11:42:54 +0100

    Lieferant changed

--- Backend/src/main/java/com/ebdapo/backend/repository/LieferantRepository.java
+++ Backend/src/main/java/com/ebdapo/backend/repository/LieferantRepository.java
@@ -2,6 +2,9 @@ package com.ebdapo.backend.repository;
 
 import com.ebdapo.backend.entity.Lieferant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface LieferantRepository extends JpaRepository<Lieferant, String> {
+    @Query(value = "SELECT * FROM apotheke a WHERE name = ?1 AND anschrift IN (SELECT id FROM adresse_t WHERE strasse = ?2 AND nummer = ?3 AND ort = ?4 and plz = ?5)", nativeQuery = true)
+    Lieferant getLieferantByValues(String name, String strasse, String nummer, String ort, int plz);
 }
