8d5202ce69b800d3c2afd50820d4f654b048769e
AdrianBeilharz <abeilharz9@gmail.com>
2020-12-07 18:29:01 +0100

    docker rules the world

--- Datenbank/fill_data.sh
+++
@@ -1,3 +0,0 @@
-docker exec -it edb-apo_db_1 mysql -uedbapo -pedbapoadmin -e "use edb-apo-db; $(cat EDB-Apo_dbdec.sql)"
-docker exec -it edb-apo_db_1 mysql -uedbapo -pedbapoadmin -e "use edb-apo-db; $(cat EDB-Apo_dbdata.sql)"
-
