8e662d518e96d6213d06ed24b24a3fd681bfa32e
AdrianBeilharz <abeilharz9@gmail.com>
2021-01-07 13:57:11 +0100

    change password field, error message on wrong credentials

--- frontend/src/components/startseite/Login.js
+++ frontend/src/components/startseite/Login.js
@@ -1,25 +1,24 @@
 import React, { useState } from 'react';
-import {Row, Col, Button, Form,} from 'react-bootstrap';
-import { useForm } from "react-hook-form";
+import {Col, Button, Form,} from 'react-bootstrap';
 import ApothekeRegisterModal from '../../modals/ApothekeRegisterModal';
+import { useSnackbar } from 'notistack';
 import './Startseite.scss'
 
 
 function Login(props) {
     const [neuesApoRegisterModal, setNeuesApoRegisterModal] = useState(false);
-    const {handleSubmit} = useForm();
-    const [user, setUser] = useState({username:'', password:''});
+    const { enqueueSnackbar } = useSnackbar();
     
     const login = async event => {
+        event.preventDefault();
+        let {username, password} = event.target;
+        let body = {username: username.value, password: password.value};
         const response = await fetch(`http://${process.env.REACT_APP_BACKEND_URL}/login`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                username: user.username,
-                password: user.password
-            })
+            body: JSON.stringify(body)
         }).catch((err) => {
             //SHOW ERROR
             console.log(err);
@@ -30,17 +29,19 @@ function Login(props) {
             const data = await response.json();
             window.sessionStorage.setItem("edbapo-jwt", data.jwt)
             props.history.push(`/apotheke/${data.apothekeId}`);
+        } else {
+            enqueueSnackbar("Nutzername oder Passwort ist Falsch", {variant: "error", autoHideDuration: 3000, anchorOrigin: {vertical: 'bottom', horizontal: 'left'}});
         }
     }
 
     return (
         <div className="login">
             <b style={{fontSize:'20pt'}}>Login:</b>
-            <Form onSubmit={handleSubmit(login)} >
+            <Form onSubmit={login} >
                 <Form.Row>
                     <Col>
-                        <Form.Control onChange={e => setUser({...user, username: e.target.value})} placeholder="Benutzername" />
-                        <Form.Control onChange={e => setUser({...user, password: e.target.value})} placeholder="Passwort" />
+                        <Form.Control name="username" placeholder="Benutzername" required />
+                        <Form.Control name="password" placeholder="Passwort" type="password" required />
                         <Button variant="primary" type="submit">Login</Button>    
                         <Button variant="primary"  show={neuesApoRegisterModal} onClick={() => setNeuesApoRegisterModal(true)}>Neue Apotheke registrieren</Button>
                         <ApothekeRegisterModal
